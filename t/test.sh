a857bbe425b905d46438bb854fce2e97c6c3e674
Nils K <git@nkn-it.de>
2022-02-16 15:18:03 +0100

    test n2-standard

--- t/test.sh
+++ t/test.sh
@@ -37,7 +37,12 @@ MY_CHECKS=(
 	'europe-west4;vm;m2-ultramem-416-1y;389'
 	# Known bug > Google Cloud Pricing Calculator: 22371 vs 22374
 	'europe-west4;vm;m2-ultramem-416-3y;223'
+	#
+	# Test VM instance pricing
+	# Price list: https://cloud.google.com/compute/vm-instance-pricing
+	#
 	# General-purpose machine type family
+	# https://cloud.google.com/compute/vm-instance-pricing#general-purpose_machine_type_family
 	# E2 standard machine types
 	'europe-west4;vm;e2-standard-2;53'
 	'europe-west4;vm;e2-standard-4;107'
@@ -55,6 +60,19 @@ MY_CHECKS=(
 	'europe-west4;vm;e2-highcpu-8;159'
 	'europe-west4;vm;e2-highcpu-16;318'
 	'europe-west4;vm;e2-highcpu-32;636'
+	# N2 standard machine types
+	'europe-west4;vm;n2-standard-2;62'
+	'europe-west4;vm;n2-standard-4;124'
+	'europe-west4;vm;n2-standard-8;249'
+	'europe-west4;vm;n2-standard-16;499'
+	# n2-standard-32 ... 128: Tested with Google Cloud Pricing Calculator
+	#                         Price differs slightly from the price list
+	'europe-west4;vm;n2-standard-32;999'
+	'europe-west4;vm;n2-standard-48;1498'
+	'europe-west4;vm;n2-standard-64;1998'
+	'europe-west4;vm;n2-standard-80;2498'
+	'europe-west4;vm;n2-standard-96;2997'
+	'europe-west4;vm;n2-standard-128;3997'
 	# Standard storage
 	'europe-west4;bucket;bucket-standard;1.0'
 	'europe-multi;bucket;bucket-standard-multi;1.3'
