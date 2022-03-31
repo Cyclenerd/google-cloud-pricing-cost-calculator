a39f6c56bb8f88118fae369a8ee85ce57f65dee1
Cyclenerd <git@nkn-it.de>
2022-03-31 10:50:30 +0200

    shorter message

--- tools/check.sh
+++ tools/check.sh
@@ -3,6 +3,8 @@
 #
 # Check CSV files and create a new issue on change
 #
+# Diff message based on https://stackoverflow.com/a/25498422
+#
 
 source "config.sh" || exit 9
 
@@ -16,53 +18,125 @@ if [ -v GITHUB_RUN_ID ]; then
 fi
 
 # Region
+MY_GITHUB_REGION_BODY="/tmp/regions.txt"
 if ! git diff --exit-code "$CSV_GCLOUD_REGIONS"; then
 	echo "'$CSV_GCLOUD_REGIONS' changed!"
-	echo "» Create a new incident to notify $GITHUB_ISSUE_ASSIGNEE."
-	git diff "$CSV_GCLOUD_REGIONS" | gh issue create --assignee "$GITHUB_ISSUE_ASSIGNEE" --label "$GITHUB_ISSUE_LABEL" --title "Change detected: Regions" --body-file -
+	{
+		echo "Regions '$CSV_GCLOUD_REGIONS' changed"
+		echo ""
+		echo "Added:"
+		git diff --color=always "$CSV_GCLOUD_REGIONS" | perl -wlne 'print $1 if /^\e\[32m\+\e\[m\e\[32m(.*)\e\[m$/'
+		echo ""
+		echo "Deleted:"
+		git diff --color=always "$CSV_GCLOUD_REGIONS" | perl -wlne 'print $1 if /^\e\[31m-(.*)\e\[m$/'
+	} > "$MY_GITHUB_REGION_BODY"
+
+	echo "» Create a new incident to notify '$GITHUB_ISSUE_ASSIGNEE'."
+	gh issue create --assignee "$GITHUB_ISSUE_ASSIGNEE" --label "$GITHUB_ISSUE_LABEL" --title "Change detected: Regions" -F "$MY_GITHUB_REGION_BODY"
 	echo "» Also create a new comment to incident '$GITHUB_ISSUE_ID_REGION'."
-	git diff "$CSV_GCLOUD_REGIONS" | gh issue comment "$GITHUB_ISSUE_ID_REGION" --body-file -
+	gh issue comment "$GITHUB_ISSUE_ID_REGION" -F "$MY_GITHUB_REGION_BODY"
+
 	git add "$CSV_GCLOUD_REGIONS"
 	((MY_CHANGES++));
 fi
+MY_GITHUB_MACHINE_TYPE_REGION_BODY="/tmp/machinetyperegion.txt"
 if ! git diff --exit-code "$CSV_GCLOUD_MACHINE_TYPE_REGION"; then
 	echo "'$CSV_GCLOUD_MACHINE_TYPE_REGION' changed!"
+	{
+		echo "Machine type in region '$CSV_GCLOUD_MACHINE_TYPE_REGION' changed"
+		echo ""
+		echo "Added:"
+		git diff --color=always "$CSV_GCLOUD_MACHINE_TYPE_REGION" | perl -wlne 'print $1 if /^\e\[32m\+\e\[m\e\[32m(.*)\e\[m$/'
+		echo ""
+		echo "Deleted:"
+		git diff --color=always "$CSV_GCLOUD_MACHINE_TYPE_REGION" | perl -wlne 'print $1 if /^\e\[31m-(.*)\e\[m$/'
+	} > "$MY_GITHUB_MACHINE_TYPE_REGION_BODY"
+
 	echo "» Create a new comment to incident '$GITHUB_ISSUE_ID_REGION'."
-	git diff "$CSV_GCLOUD_MACHINE_TYPE_REGION" | gh issue comment "$GITHUB_ISSUE_ID_REGION" --body-file -
+	gh issue comment "$GITHUB_ISSUE_ID_REGION" -F "$MY_GITHUB_MACHINE_TYPE_REGION_BODY"
+
 	git add "$CSV_GCLOUD_MACHINE_TYPE_REGION"
 	((MY_CHANGES++));
 fi
 
 # Zones
+MY_GITHUB_ZONES_BODY="/tmp/zones.txt"
 if ! git diff --exit-code "$CSV_GCLOUD_ZONES"; then
 	echo "'$CSV_GCLOUD_ZONES' changed!"
+	{
+		echo "Zones '$CSV_GCLOUD_ZONES' changed"
+		echo ""
+		echo "Added:"
+		git diff --color=always "$CSV_GCLOUD_ZONES" | perl -wlne 'print $1 if /^\e\[32m\+\e\[m\e\[32m(.*)\e\[m$/'
+		echo ""
+		echo "Deleted:"
+		git diff --color=always "$CSV_GCLOUD_ZONES" | perl -wlne 'print $1 if /^\e\[31m-(.*)\e\[m$/'
+	} > "$MY_GITHUB_ZONES_BODY"
+
 	echo "» Create a new comment to incident '$GITHUB_ISSUE_ID_ZONE'."
-	git diff "$CSV_GCLOUD_ZONES" | gh issue comment "$GITHUB_ISSUE_ID_ZONE" --body-file -
+	gh issue comment "$GITHUB_ISSUE_ID_ZONE" -F "$MY_GITHUB_ZONES_BODY"
+
 	git add "$CSV_GCLOUD_ZONES"
 	((MY_CHANGES++));
 fi
+MY_GITHUB_MACHINE_TYPE_ZONE_BODY="/tmp/machinetypezone.txt"
 if ! git diff --exit-code "$CSV_GCLOUD_MACHINE_TYPE_ZONE"; then
 	echo "'$CSV_GCLOUD_MACHINE_TYPE_ZONE' changed!"
+	{
+		echo "Machine type in zone '$CSV_GCLOUD_MACHINE_TYPE_ZONE' changed"
+		echo ""
+		echo "Added:"
+		git diff --color=always "$CSV_GCLOUD_MACHINE_TYPE_ZONE" | perl -wlne 'print $1 if /^\e\[32m\+\e\[m\e\[32m(.*)\e\[m$/'
+		echo ""
+		echo "Deleted:"
+		git diff --color=always "$CSV_GCLOUD_MACHINE_TYPE_ZONE" | perl -wlne 'print $1 if /^\e\[31m-(.*)\e\[m$/'
+	} > "$MY_GITHUB_MACHINE_TYPE_ZONE_BODY"
+
 	echo "» Create a new comment to incident '$GITHUB_ISSUE_ID_ZONE'."
-	git diff "$CSV_GCLOUD_MACHINE_TYPE_ZONE" | gh issue comment "$GITHUB_ISSUE_ID_ZONE" --body-file -
+	gh issue comment "$GITHUB_ISSUE_ID_ZONE" -F "$MY_GITHUB_MACHINE_TYPE_ZONE_BODY"
+
 	git add "$CSV_GCLOUD_MACHINE_TYPE_ZONE"
 	((MY_CHANGES++));
 fi
 
 # Disk types
+MY_GITHUB_DISK_TYPES_BODY="/tmp/disktypes.txt"
 if ! git diff --exit-code "$CSV_GCLOUD_DISK_TYPES"; then
 	echo "'$CSV_GCLOUD_DISK_TYPES' changed!"
-	echo "»  Create a new incident to notify $GITHUB_ISSUE_ASSIGNEE."
-	git diff "$CSV_GCLOUD_DISK_TYPES" | gh issue create --assignee "$GITHUB_ISSUE_ASSIGNEE" --label "$GITHUB_ISSUE_LABEL" --title "Change detected: Disk types" --body-file -
+	{
+		echo "Disk types '$CSV_GCLOUD_DISK_TYPES' changed"
+		echo ""
+		echo "Added:"
+		git diff --color=always "$CSV_GCLOUD_MACHINE_TYPES" | perl -wlne 'print $1 if /^\e\[32m\+\e\[m\e\[32m(.*)\e\[m$/'
+		echo ""
+		echo "Deleted:"
+		git diff --color=always "$CSV_GCLOUD_MACHINE_TYPES" | perl -wlne 'print $1 if /^\e\[31m-(.*)\e\[m$/'
+	} > "$MY_GITHUB_DISK_TYPES_BODY"
+
+	echo "»  Create a new incident to notify '$GITHUB_ISSUE_ASSIGNEE'."
+	gh issue create --assignee "$GITHUB_ISSUE_ASSIGNEE" --label "$GITHUB_ISSUE_LABEL" --title "Change detected: Disk types" -F "$MY_GITHUB_DISK_TYPES_BODY"
+
 	git add "$CSV_GCLOUD_DISK_TYPES"
 	((MY_CHANGES++));
 fi
 
 # Machine types
+MY_GITHUB_MACHINE_TYPES_BODY="/tmp/machinetypes.txt"
 if ! git diff --exit-code "$CSV_GCLOUD_MACHINE_TYPES"; then
 	echo "'$CSV_GCLOUD_MACHINE_TYPES' changed!"
-	echo "» Create a new incident to notify $GITHUB_ISSUE_ASSIGNEE."
-	git diff "$CSV_GCLOUD_MACHINE_TYPES" | gh issue create --assignee "$GITHUB_ISSUE_ASSIGNEE" --label "$GITHUB_ISSUE_LABEL" --title "Change detected: Machine types" --body-file -
+	{
+		echo "Machine types '$CSV_GCLOUD_MACHINE_TYPES' changed"
+		echo ""
+		echo "Added:"
+		git diff --color=always "$CSV_GCLOUD_MACHINE_TYPES" | perl -wlne 'print $1 if /^\e\[32m\+\e\[m\e\[32m(.*)\e\[m$/'
+		echo ""
+		echo "Deleted:"
+		git diff --color=always "$CSV_GCLOUD_MACHINE_TYPES" | perl -wlne 'print $1 if /^\e\[31m-(.*)\e\[m$/'
+	} > "$MY_GITHUB_MACHINE_TYPES_BODY"
+
+	echo "» Create a new incident to notify '$GITHUB_ISSUE_ASSIGNEE'."
+	gh issue create --assignee "$GITHUB_ISSUE_ASSIGNEE" --label "$GITHUB_ISSUE_LABEL" --title "Change detected: Machine types" -F "$MY_GITHUB_MACHINE_TYPES_BODY"
+
 	git add "$CSV_GCLOUD_MACHINE_TYPES"
 	((MY_CHANGES++));
 fi
