296db166063e34cbbd4c6f7b26182dc6798c64f9
Cyclenerd <git@nkn-it.de>
2022-03-31 09:05:24 +0200

    machine types without description

--- tools/machinetypes.sh
+++ tools/machinetypes.sh
@@ -8,11 +8,11 @@ source "config.sh" || exit 9
 
 echo "Get machine types... Please wait..."
 
-echo "NAME;CPUS;SHARED_CPU;MEMORY_GB;DESCRIPTION;DEPRECATED;ZONE" > "$CSV_GCLOUD_MACHINE_TYPE_ZONE" || exit 9
+echo "NAME;CPUS;SHARED_CPU;MEMORY_GB;DEPRECATED;ZONE" > "$CSV_GCLOUD_MACHINE_TYPE_ZONE" || exit 9
 gcloud compute machine-types list \
 	--quiet \
 	--filter="ZONE:-" \
-	--format="csv[no-heading,separator=';'](NAME,CPUS,isSharedCpu,MEMORY_GB,description,DEPRECATED,ZONE)" | sort -u >> "$CSV_GCLOUD_MACHINE_TYPE_ZONE" || exit 9
+	--format="csv[no-heading,separator=';'](NAME,CPUS,isSharedCpu,MEMORY_GB,DEPRECATED,ZONE)" | sort -u >> "$CSV_GCLOUD_MACHINE_TYPE_ZONE" || exit 9
 
 cp "$CSV_GCLOUD_MACHINE_TYPE_ZONE" "$CSV_GCLOUD_MACHINE_TYPE_REGION" || exit 9
 perl -i -pe's/-[a-z]$//g' "$CSV_GCLOUD_MACHINE_TYPE_REGION" || exit 9 # Remove zone (-a ... -z)
