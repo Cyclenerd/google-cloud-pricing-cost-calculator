bd1901936e26e91083fd202a7ba091a30d11ebf6
Cyclenerd <git@nkn-it.de>
2022-04-21 16:30:35 +0200

    images

--- tools/config.sh
+++ tools/config.sh
@@ -12,6 +12,7 @@ CSV_GCLOUD_ACCELERATOR_TYPE_ZONE="acceleratortypezone.csv"
 CSV_GCLOUD_ACCELERATOR_TYPE_REGION="acceleratortyperegion.csv"
 CSV_GCLOUD_ACCELERATOR_TYPES="acceleratortypes.csv"
 CSV_GCLOUD_IMAGES="images.csv"
+CSV_GCLOUD_COMMUNITY_IMAGES="imagescommunity.csv"
 
 GITHUB_ISSUE_ASSIGNEE="Cyclenerd"
 GITHUB_ISSUE_LABEL="new"
--- tools/images.sh
+++ tools/images.sh
@@ -8,9 +8,37 @@ source "config.sh" || exit 9
 
 echo "Get images... Please wait..."
 
-echo "NAME;DESCRIPTION;DISK_SIZE_GB;PROJECT;FAMILY;CREATION;STATUS" > "$CSV_GCLOUD_IMAGES" || exit 9
+echo "NAME;DESCRIPTION;DISK_SIZE_GB;PROJECT;FAMILY;CREATION;DEPRECATED;STATUS" > "$CSV_GCLOUD_IMAGES" || exit 9
 gcloud compute images list \
 	--quiet \
-	--format="csv[no-heading,separator=';'](NAME,description,diskSizeGb,PROJECT,FAMILY,creationTimestamp,STATUS)" | sort -u >> "$CSV_GCLOUD_IMAGES" || exit 9
+	--format="csv[no-heading,separator=';'](NAME,description,diskSizeGb,PROJECT,FAMILY,creationTimestamp,DEPRECATED,STATUS)" | sort -u >> "$CSV_GCLOUD_IMAGES" || exit 9
+
+echo "Get community images... Please wait..."
+
+echo "NAME;DESCRIPTION;DISK_SIZE_GB;PROJECT;FAMILY;CREATION;DEPRECATED;STATUS" > "$CSV_GCLOUD_COMMUNITY_IMAGES" || exit 9
+# https://cloud.google.com/compute/docs/images#almalinux
+gcloud compute images list \
+	--project almalinux-cloud \
+	--no-standard-images \
+	--quiet \
+	--format="csv[no-heading,separator=';'](NAME,description,diskSizeGb,PROJECT,FAMILY,creationTimestamp,DEPRECATED,STATUS)" | sort -u >> "$CSV_GCLOUD_COMMUNITY_IMAGES" || exit 9
+# https://cloud.google.com/compute/docs/images#fedora_cloud
+gcloud compute images list \
+	--project fedora-cloud \
+	--no-standard-images \
+	--quiet \
+	--format="csv[no-heading,separator=';'](NAME,description,diskSizeGb,PROJECT,FAMILY,creationTimestamp,DEPRECATED,STATUS)" | sort -u >> "$CSV_GCLOUD_COMMUNITY_IMAGES" || exit 9
+# https://cloud.google.com/compute/docs/images#freebsd
+gcloud compute images list \
+	--project freebsd-org-cloud-dev \
+	--no-standard-images \
+	--quiet \
+	--format="csv[no-heading,separator=';'](NAME,description,diskSizeGb,PROJECT,FAMILY,creationTimestamp,DEPRECATED,STATUS)" | sort -u >> "$CSV_GCLOUD_COMMUNITY_IMAGES" || exit 9
+# https://cloud.google.com/compute/docs/images#opensuse
+gcloud compute images list \
+	--project opensuse-cloud \
+	--no-standard-images \
+	--quiet \
+	--format="csv[no-heading,separator=';'](NAME,description,diskSizeGb,PROJECT,FAMILY,creationTimestamp,DEPRECATED,STATUS)" | sort -u >> "$CSV_GCLOUD_COMMUNITY_IMAGES" || exit 9
 
 echo "DONE"
